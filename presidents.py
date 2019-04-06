f8fcd5cd7cf84989834f088a802f3697cbcdb0d2
hamdyaea <hamdy.aea@gmail.com>
2019-04-06 18:56:35 +0200

    urllib

--- presidents.py
+++ presidents.py
@@ -5,20 +5,24 @@
 # This software is not finish I am working on it. I publish it to test it.
 
 import json
+import urllib.request
 from easygui import *
 
 def parser():
-    with open('https://raw.githubusercontent.com/hamdyaea/USA-Presidents-history/master/presidents.json') as json_file:
-        data = json.load(json_file)
-        print(data[1]["number"])
-        print(data[1]["president"])
-        print(data[1]["birth_year"])
-        print(data[1]["death_year"])
-        print(data[1]["took_office"])
-        print(data[1]["left_office"])
-        print(data[1]["party"])
-        #print(data[0]["picture"])
-        #print(data[0]["history"])
-        print(len(data))
+    urlData = ("https://raw.githubusercontent.com/hamdyaea/USA-Presidents-history/master/presidents.json")
+    webURL = urllib.request.urlopen(urlData)
+    data = webURL.read()
+    encoding = webURL.info().get_content_charset('utf-8')
+    presid = json.loads(data.decode(encoding))
+    print(presid[1]["number"])
+    print(presid[1]["president"])
+    print(presid[1]["birth_year"])
+    print(presid[1]["death_year"])
+    print(presid[1]["took_office"])
+    print(presid[1]["left_office"])
+    print(presid[1]["party"])
+    #print(presid[0]["picture"])
+    #print(presid[0]["history"])
+    print(len(presid))
 
 parser()
