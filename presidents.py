da633f89c1711de4aa3101bb9f66c4ecd21b0e74
hamdyaea <hamdy.aea@gmail.com>
2019-04-06 19:01:13 +0200

    pic link

--- presidents.py
+++ presidents.py
@@ -14,15 +14,22 @@ def parser():
     data = webURL.read()
     encoding = webURL.info().get_content_charset('utf-8')
     presid = json.loads(data.decode(encoding))
-    print(presid[1]["number"])
-    print(presid[1]["president"])
-    print(presid[1]["birth_year"])
-    print(presid[1]["death_year"])
-    print(presid[1]["took_office"])
-    print(presid[1]["left_office"])
-    print(presid[1]["party"])
-    #print(presid[0]["picture"])
-    #print(presid[0]["history"])
+    print(presid[0]["number"])
+    print(presid[0]["president"])
+    print(presid[0]["birth_year"])
+    print(presid[0]["death_year"])
+    print(presid[0]["took_office"])
+    print(presid[0]["left_office"])
+    print(presid[0]["party"])
+    print(presid[0]["picture"])
+    print(presid[0]["history"])
     print(len(presid))
+
+    image = presid[0]["picture"]
+    msg = presid[0]["president"]
+    choices = ["Yes", "No", "No opinion"]
+    reply = buttonbox(msg, image=image, choices=choices)
+
+
 
 parser()
